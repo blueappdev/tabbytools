4e3ad1bc3f2cc2b8f29726d9f066b7955988ee5e
blueappdev <appdev@bluewin.ch>
2019-02-20 06:37:16 +0100

    tablist indicator

--- lib/tableprinter.py
+++ lib/tableprinter.py
@@ -20,77 +20,6 @@ class Formatter:
     def __init__(self, someArguments):
         self.options, self.arguments = getopt.getopt(someArguments, "s:hl")
 
-    def usage(self):
-        print "tabby.py: extract excel data"
-        print "usage: tabby.py [-h] [-s num] file [ file...]"
-        print "  -h - help"
-        print "  -l - list of key value pairs"
-        print "  -s num (integer)"
-        print "    extract worksheet with index num,"
-        print "    e.g -s 2 extracts the second workseet"
-
-    def setWorksheetsToExtractFromOptionValue(self, optionValue):
-        self.indexesToExtract = [ int(optionValue) ]
-
-    def processFile(self, aFilename):
-        self.workbook = tableparser.FileReaderInterface(aFilename).getWorkbook()
-        indexes = self.indexesToExtract
-        if indexes is None:
-            indexes = range(1, len(self.workbook.sheets) + 1)
-        #print "indexes", indexes
-        for each in indexes:
-            sheet = self.getSheetWithIndex(each)
-            if sheet is None:
-                exit("worksheet", str(each), "not found")
-            else:
-                self.outputSheet(sheet)
-        self.counter += 1
-    
-    # The index is based on one and not on zero.
-    def getSheetWithIndex(self, index):
-        sheets = self.workbook.sheets
-        if 1 <= index <= len(sheets):
-            return sheets[index-1]       
-        else:
-            return None
-
-    def outputSheet(self, aSheet):
-        if self.listMode:
-            self.listSheet(aSheet)
-        else:
-            self.printSheet(aSheet)
-            
-    def listSheet(self, aSheet):
-        for each in aSheet.records:
-            self.listRecord(each)
-            
-    def listRecord(self, aRecord):
-        for each in aRecord:
-            self.listCell(each)
-            
-    def listCell(self, aCell):
-        print aCell
-        
-    #
-    # A sheet is a list of records.
-    #
-    def printSheet(self, aSheet):
-        maxRecordLength = 0
-        for each in aSheet.records:
-            maxRecordLength = max(maxRecordLength, len(each))
-        widths = [0] * maxRecordLength      # array of zeros [0,0,0,0 ...]
-        for each in aSheet.records:
-            for index, value in enumerate(each):
-                widths[index] = max(widths[index], len(value))
-        for each in aSheet.records:
-            for index, value in enumerate(each):
-                alphabeticCharacters = filter(lambda ch: ch.isalpha(), value)
-                if alphabeticCharacters == "":
-                    print value.rjust(widths[index]),
-                else:
-                    print value.ljust(widths[index]),
-            print
-            
     def process(self):
         self.indexesToExtract = None  # by default extract all worksheets
         self.listMode = False
@@ -114,5 +43,83 @@ class Formatter:
             for file in glob.glob(pattern):
                 self.processFile(file)
         if self.counter == 0:
-            exit("no files found")
+            exit("no files found")        
+        
+    def usage(self):
+        print "tabby.py: extract excel data"
+        print "usage: tabby.py [-h] [-s num] file [ file...]"
+        print "  -h - help"
+        print "  -l - list of key value pairs"
+        print "  -s num (integer)"
+        print "    extract worksheet with index num,"
+        print "    e.g -s 2 extracts the second workseet"
 
+    def setWorksheetsToExtractFromOptionValue(self, optionValue):
+        self.indexesToExtract = [ int(optionValue) ]
+
+    def processFile(self, aFilename):
+        self.workbook = tableparser.FileReaderInterface(aFilename).getWorkbook()
+        indexes = self.indexesToExtract
+        if indexes is None:
+            indexes = range(1, len(self.workbook.sheets) + 1)
+        #print "indexes", indexes
+        for sheetIndex in indexes:
+            sheet = self.getSheetWithIndex(sheetIndex)
+            if sheet is None:
+                exit("worksheet", str(each), "not found")
+            else:
+                self.outputSheet(sheetIndex, sheet)
+        self.counter += 1
+    
+    # The index is based on one and not on zero.
+    def getSheetWithIndex(self, index):
+        sheets = self.workbook.sheets
+        if 1 <= index <= len(sheets):
+            return sheets[index-1]       
+        else:
+            return None
+
+    def outputSheet(self, sheetIndex, aSheet):
+        if self.listMode:
+            self.listSheet(sheetIndex, aSheet)
+        else:
+            self.printSheet(aSheet)
+            
+    def listSheet(self, sheetNumber, aSheet):
+        for rowNumber, each in enumerate(aSheet.records):
+            self.listRecord(sheetNumber, rowNumber, each)
+            
+    def listRecord(self, sheetNumber, rowNumber, aRecord):
+        for columnNumber, each in enumerate(aRecord):
+            self.listCell(sheetNumber, rowNumber, columnNumber, each)
+            
+    def listCell(self, sheetNumber, rowNumber, columnNumber, aCell):
+        prefix = str(sheetNumber) + "." + self.excelReference(rowNumber+1, columnNumber+1) +  ":"
+        print prefix, aCell
+        
+    #
+    # A sheet is a list of records.
+    #
+    def printSheet(self, aSheet):
+        maxRecordLength = 0
+        for each in aSheet.records:
+            maxRecordLength = max(maxRecordLength, len(each))
+        widths = [0] * maxRecordLength      # array of zeros [0,0,0,0 ...]
+        for each in aSheet.records:
+            for index, value in enumerate(each):
+                widths[index] = max(widths[index], len(value))
+        for each in aSheet.records:
+            for index, value in enumerate(each):
+                alphabeticCharacters = filter(lambda ch: ch.isalpha(), value)
+                if alphabeticCharacters == "":
+                    print value.rjust(widths[index]),
+                else:
+                    print value.ljust(widths[index]),
+            print
+            
+    # Convert given row and column number to an Excel-style cell name.
+    def excelReference(self, row, column):
+        quot, rem = divmod(column - 1, 26)
+        return((chr(quot-1 + ord('A')) if quot else '') +
+               (chr(rem + ord('A')) + str(row)))
+            
